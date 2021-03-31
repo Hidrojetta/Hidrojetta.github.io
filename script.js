8c0c2daef7389739812f31e31c5ce30aa0d8d8bf
Hidrojetta <81699779+Hidrojetta@users.noreply.github.com>
2021-03-31 12:21:16 -0300

    site-upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function button (code) {
+ if (code == 0) {
+ //window.open("tel:998318515",target="_blank")
+ window.location.replace("tel:998318515")
+}
+ if(code == 1) {
+  window.location.replace("#downdiv")
+ }
+}
